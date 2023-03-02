99475141655cfb56f3a0827a0cf9fe2ce85b9508
v.vivetha <81745660+vivethav@users.noreply.github.com>
2023-03-02 12:53:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
  public static void main(String[]args)
  {
-  System.out.println("......*****Hello Vivetha Welcome to Jenkins Training*****......");
+  System.out.println("......*****Made some changes Vivetha Welcome to Jenkins Training*****......");
  }
 }
